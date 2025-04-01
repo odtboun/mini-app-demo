feb33226c849b6c8b8c562baf3ea038fadb41e43
odtboun <54063336+odtboun@users.noreply.github.com>
2025-04-01 11:47:07 +0300

    fix: add imagedelivery.net to Next.js image domains

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,16 @@ const nextConfig = {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'imagedelivery.net',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
